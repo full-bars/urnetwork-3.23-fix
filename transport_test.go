088b7ebf61e82b51715cf4fc125a4a2a9131524a
full-bars <45684698+full-bars@users.noreply.github.com>
2026-06-30 18:39:15 -0700

    test: add regression test for modeMonitor NotifyAll

--- transport_test.go
+++ transport_test.go
@@ -2,6 +2,7 @@ package connect
 
 import (
 	"testing"
+	"time"
 
 	"github.com/go-playground/assert/v2"
 )
@@ -25,4 +26,28 @@ func TestConnectPumpHost(t *testing.T) {
 	host, err = pumpHost("http://127.0.0.1", []byte("foo.com"))
 	assert.Equal(t, err, nil)
 	assert.Equal(t, host, "127.0.0.1")
+}
+
+func TestSetModeAvailableWakesWaiter(t *testing.T) {
+	transport := &PlatformTransport{
+		modeMonitor:    NewMonitor(),
+		availableModes: map[TransportMode]bool{},
+	}
+
+	available, notify := transport.modeAvailable(TransportModeH1)
+	assert.Equal(t, false, available)
+
+	woke := make(chan struct{})
+	go func() {
+		<-notify
+		close(woke)
+	}()
+
+	transport.setModeAvailable(TransportModeH1, true)
+
+	select {
+	case <-woke:
+	case <-time.After(1 * time.Second):
+		t.Fatal("waiter was not notified after setModeAvailable")
+	}
 }
